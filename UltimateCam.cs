19b32309f273caf8396f1be6bb853fc83f9f3813
V10lator <v10lator@myway.de>
2016-07-12 10:47:48 +0200

    Fix copy&paste error.
    Signed-off-by: V10lator <v10lator@myway.de>

--- UltimateCam.cs
+++ UltimateCam.cs
@@ -37,7 +37,7 @@ namespace UltimateCam
 					}
 				} else
 					LeaveHeadCam ();
-			} else if (Input.GetMouseButtonUp (FpsMouse.MOUSEBUTTON.LEFT)) {
+			} else if (Input.GetMouseButtonUp ((int)FpsMouse.MOUSEBUTTON.LEFT)) {
 				if (active) {
 					Utility.ObjectBelowMouseInfo result = default(Utility.ObjectBelowMouseInfo);
 					Ray ray = _cam.ScreenPointToRay (Input.mousePosition);
@@ -92,7 +92,7 @@ namespace UltimateCam
 							EnterCoasterCam (attr);
 					}
 				}
-			} else if (Input.GetMouseButtonUp (FpsMouse.MOUSEBUTTON.RIGHT) && active && riding)
+			} else if (Input.GetMouseButtonUp ((int)FpsMouse.MOUSEBUTTON.RIGHT) && active && riding)
 				LeaveCoasterCam ();
 
 			if (active) {
