d024fe25cfc47173e8019591c7843e680e74fa3f
V10lator <v10lator@myway.de>
2016-07-23 14:24:17 +0200

    Limit horizontal head movement while riding an attraction.
    Signed-off-by: V10lator <v10lator@myway.de>

--- FpsMouse.cs
+++ FpsMouse.cs
@@ -13,6 +13,7 @@ namespace UltimateCam
 
 		private float _sensitivity = 10.0F;
 		private float _yRad = 180.0f;
+		private float _xRad = 222.22f;
 
 		private float yaw = 0.0f;
 		private float pitch = 0.0f;
@@ -29,24 +30,33 @@ namespace UltimateCam
 		void Update()
 		{
 			yaw += Input.GetAxis ("Mouse X") * _sensitivity;
-			yaw = keepInCircle (yaw);
-			float tmp = pitch - (Input.GetAxis ("Mouse Y") * _sensitivity);
-			float yh = _yRad / 2.0f;
-			if (tmp > yh)
-				pitch = yh;
-			else if (tmp < -yh)
-				pitch = -yh;
-			else
-				pitch = tmp;
+			pitch -= Input.GetAxis ("Mouse Y") * _sensitivity;
+
+			// Limit vertical head movement
+			float h = _yRad / 2.0f;
+			if (pitch > h)
+				pitch = h;
+			else if (pitch < -h)
+				pitch = -h;
 
 			Vector3 euler;
 			if (UltimateCam.riding)
 			{
+				// Limit horizontal head movement
+				h = _xRad / 2.0f;
+				if (yaw > h)
+					yaw = h;
+				else if (yaw < -h)
+					yaw = -h;
+				
 				euler = transform.parent.eulerAngles;
 				euler = new Vector3(keepInCircle(euler.x + pitch), keepInCircle(euler.y + yaw), euler.z);
 			}
 			else
+			{
+				yaw = keepInCircle(yaw);
 				euler = new Vector3(pitch, yaw, 0.0f);
+			}
 			
 			transform.eulerAngles = euler;
 		}
