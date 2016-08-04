0aca79199222873e8ba1dad39c52f3b60c53627e
V10lator <v10lator@myway.de>
2016-08-04 06:22:24 +0200

    PlayerController: Faster config access.
    Signed-off-by: V10lator <v10lator@myway.de>

--- PlayerController.cs
+++ PlayerController.cs
@@ -10,6 +10,7 @@ namespace UltimateCam
 		private float upSpeed = 0.0f;
 
 		private Vector3 moveDirection = Vector3.zero;
+		private UltimateSettings config;
 		private CharacterController controller;
 
 		private bool underground = false;
@@ -17,8 +18,9 @@ namespace UltimateCam
 		// Use this for initialization
 		void Start()
 		{
-			speed = UltimateMain.Instance.config.WalkingSpeed;
-			gravity = UltimateMain.Instance.config.Gravity;
+			config = UltimateMain.Instance.config;
+			speed = config.WalkingSpeed;
+			gravity = config.Gravity;
 
 			controller = GetComponent<CharacterController>();
 			controller.detectCollisions = controller.enableOverlapRecovery = true;
@@ -27,9 +29,9 @@ namespace UltimateCam
 		// Update is called once per frame
 		void Update()
 		{
-			if (Input.GetKey(UltimateMain.Instance.config.GetKey(UltimateSettings.ROTATE_LEFT_KEY_SETTING)))
+			if (Input.GetKey(config.GetKey(UltimateSettings.ROTATE_LEFT_KEY_SETTING)))
 				Camera.main.gameObject.GetComponent<UltimateMouse>().yaw -= speed * 50.0f * Time.deltaTime;
-			else if(Input.GetKey(UltimateMain.Instance.config.GetKey(UltimateSettings.ROTATE_RIGHT_KEY_SETTING)))
+			else if(Input.GetKey(config.GetKey(UltimateSettings.ROTATE_RIGHT_KEY_SETTING)))
 				Camera.main.gameObject.GetComponent<UltimateMouse>().yaw += speed * 50.0f * Time.deltaTime;
 
 			// Detect tunnels
@@ -87,20 +89,20 @@ namespace UltimateCam
 				grounded = false;
 
 			bool falling;
-			if (grounded || UltimateMain.Instance.config.Jetpack)
+			if (grounded || config.Jetpack)
 			{
 				moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
 				moveDirection = transform.TransformDirection(moveDirection);
 				moveDirection *= speed * Time.deltaTime;
 
-				bool jump = Input.GetKey(UltimateMain.Instance.config.GetKey(UltimateSettings.JUMP_KEY_SETTING));
+				bool jump = Input.GetKey(config.GetKey(UltimateSettings.JUMP_KEY_SETTING));
 				if (grounded)
 				{
 					upSpeed = jump ? 0.1f : 0.0f;
 					moveDirection.y = upSpeed * Time.deltaTime;
 					falling = false;
 				}
-				else // UltimateMain.Instance.config.Jetpack
+				else // config.Jetpack
 				{
 					if (jump)
 					{
@@ -121,7 +123,7 @@ namespace UltimateCam
 			moveDirection.y = upSpeed;
 
 			//EXPERIMENTAL: More collissions...
-			if (UltimateMain.Instance.config.MoreCols) {
+			if (config.MoreCols) {
 				Utility.ObjectBelowMouseInfo result = default(Utility.ObjectBelowMouseInfo);
 				Ray ray = Camera.main.ScreenPointToRay (moveDirection);
 				result.hitDistance = float.MaxValue;
