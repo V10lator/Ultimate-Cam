7c413295b85058ba56b1530b4715e374b5abab81
V10lator <v10lator@myway.de>
2016-08-04 06:19:27 +0200

    Ensure overlap recovery is enabled.
    Signed-off-by: V10lator <v10lator@myway.de>

--- PlayerController.cs
+++ PlayerController.cs
@@ -21,7 +21,7 @@ namespace UltimateCam
 			gravity = config.Gravity;
 
 			controller = GetComponent<CharacterController>();
-			controller.detectCollisions = true;
+			controller.detectCollisions = controller.enableOverlapRecovery = true;
 		}
 
 		// Update is called once per frame
